9c62f44e7a14ed3206b48b1b146a288569cc9b5f
hyeon-seo <rrkdgustj727@gmail.com>
2024-09-24 02:00:28 +0900

    test commit. hello.cu/memcpy.cu

---
+++ CudaRuntime1/hello.cu
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+__global__ void hello(void) {
+    printf("hello, CUDA %d!\n", threadIdx.x);
+}
+
+int main(void) {
+    hello<<<8, 2>>>();
+    fflush(stdout);
+    return 0;
+}
